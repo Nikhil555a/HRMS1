4db7554d49bef2927b186a21a4820a46f7ac01e4
Nikhil Pandey <np8654750@gmail.com>
2026-04-28 18:57:54 +0530

    Change API base URL to production
    Updated the API base URL to point to the production server.

--- emp-management/frontend/src/utils/api.js
+++ emp-management/frontend/src/utils/api.js
@@ -22,12 +22,43 @@
 // export default api;
 
 
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: '/api' ,
+//    timeout: 120000, // ✅ 2 minute — Cloudinary upload ke liye
+//   // baseURL: 'https://hrms1-3.onrender.com/api'
+// });
+
+// api.interceptors.request.use(cfg => {
+//   const token = localStorage.getItem('token');
+//   if (token) cfg.headers.Authorization = `Bearer ${token}`;
+//   return cfg;
+// });
+
+// api.interceptors.response.use(
+//   res => res,
+//   err => {
+//     if (err.response?.status === 401) {
+//       localStorage.removeItem('token');
+//       window.location.href = '/login';
+//     }
+//     return Promise.reject(err);
+//   }
+// );
+
+// export default api;
+
+
+
+
+
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api' ,
-   timeout: 120000, // ✅ 2 minute — Cloudinary upload ke liye
-  // baseURL: 'https://hrms1-3.onrender.com/api'
+    timeout: 120000, // ✅ 2 minute — Cloudinary upload ke liye
+  baseURL: 'https://hrms1-3.onrender.com/api'
 });
 
 api.interceptors.request.use(cfg => {
